ef7e72cd331da1be7c45cf9e73cb02807571c0cc
pathum <ckpathumck@gmail.com>
2024-05-03 21:25:41 +0530

    set customer navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,22 @@ $("#btnLogin").eq(0).on('click', () => {
     $('#header').css({display: 'block'});
     $('#login').css({display: 'none'});
     $('#Item-Section').hide();
+    $('#Customer-Section').hide();
 });
 
 $('#spanHome').on('click', function() {
     $('#header').show();
     $('#Item-Section').hide();
+    $('#Customer-Section').hide();
 });
 
 $('#spanItem').on('click', function() {
     $('#Item-Section').show();
+    $('#Customer-Section').hide();
 });
+
+$('#spanCustomer').on('click', function() {
+    $('#Customer-Section').show();
+    $('#Item-Section').hide();
+});
+
