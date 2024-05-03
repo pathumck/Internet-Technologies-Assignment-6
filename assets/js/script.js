408d3466189fe44f980b0642b4360fd0c5848dff
pathum <ckpathumck@gmail.com>
2024-05-03 23:11:08 +0530

    set order navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ $("#btnLogin").eq(0).on('click', () => {
     $('#login').css({display: 'none'});
     $('#Item-Section').hide();
     $('#Customer-Section').hide();
+    $('#Order-Section').hide();
 });
 
 $('#spanHome').on('click', function() {
@@ -18,16 +19,26 @@ $('#spanHome').on('click', function() {
     $('#Home-Section').show();
     $('#Item-Section').hide();
     $('#Customer-Section').hide();
+    $('#Order-Section').hide();
 });
 
 $('#spanItem').on('click', function() {
     $('#Item-Section').show();
     $('#Home-Section').hide();
     $('#Customer-Section').hide();
+    $('#Order-Section').hide();
 });
 
 $('#spanCustomer').on('click', function() {
     $('#Customer-Section').show();
     $('#Item-Section').hide();
+    $('#Home-Section').hide();
+    $('#Order-Section').hide();
 });
 
+$('#spanOrder').on('click', function() {
+    $('#Order-Section').show();
+    $('#Customer-Section').hide();
+    $('#Item-Section').hide();
+    $('#Home-Section').hide();
+});
