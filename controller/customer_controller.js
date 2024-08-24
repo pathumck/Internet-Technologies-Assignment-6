ac8ed4a814cc0058fd8fe3092c8af39974ca49ac
pathumck <ckpathumck@gmail.com>
2024-08-24 19:49:28 +0530

    update load customer table using ajax

--- controller/customer_controller.js
+++ controller/customer_controller.js
@@ -1,7 +1,10 @@
 import {CustomerModel} from "../model/CustomerModel.js";
 import {customers, items} from "../db/db.js";
 
-validation();
+$(document).ready(function() {
+    loadTable();
+    validation();
+});
 
 $('#customer-save').click(function () {
 
@@ -26,7 +29,7 @@ $('#customer-save').click(function () {
         success: (res) => {
             console.log(JSON.stringify(res));
             alert("Customer details have been successfully saved.");
-
+            loadTable();
             $('#customer-id').val('');
             $('#customer-name').val('');
             $('#customer-address').val('');
@@ -39,62 +42,86 @@ $('#customer-save').click(function () {
     });
 });
 
-
 function loadTable() {
     var tableBody = $('#table-customer').find('tbody');
-
     tableBody.empty();
 
-    customers.forEach(function (customer, index) {
-        var row = $('<tr>');
+    $.ajax({
+        url: 'http://localhost:8080/possystem/customer',
+        type: 'GET',
+        dataType: 'json',
+        success: function(customers) {
+            customers.forEach(function (customer, index) {
+                var row = $('<tr>');
 
-        $('<td>').text(customer.id).appendTo(row);
-        $('<td>').text(customer.name).appendTo(row);
-        $('<td>').text(customer.address).appendTo(row);
-        $('<td>').text(customer.tp).appendTo(row);
+                $('<td>').text(customer.id).appendTo(row);
+                $('<td>').text(customer.name).appendTo(row);
+                $('<td>').text(customer.address).appendTo(row);
+                $('<td>').text(customer.phone).appendTo(row);
 
-        var actionCell = $('<td>').addClass('table-action-col').appendTo(row);
+                var actionCell = $('<td>').addClass('table-action-col').appendTo(row);
 
-        // Update button
-        $('<button>').text('Update').addClass('btn btn-primary btn-sm mr-1')
-            .on('click', function () {
+                // Update button
+                $('<button>').text('Update').addClass('btn btn-primary btn-sm mr-1')
+                    .on('click', function () {
+                        $('#update-customer-id').val(customer.id);
+                        $('#update-customer-name').val(customer.name);
+                        $('#update-customer-address').val(customer.address);
+                        $('#update-customer-tp').val(customer.phone);
 
-                $('#update-customer-id').val(customer.id);
-                $('#update-customer-name').val(customer.name);
-                $('#update-customer-address').val(customer.address);
-                $('#update-customer-tp').val(customer.tp);
+                        $('#updateCustomerModal').modal('show');
 
-                $('#updateCustomerModal').modal('show');
+                        $('#update-customer-btn').off('click').on('click', function () {
+                            var confirmUpdate = confirm('Are you sure you want to update this customer?');
+                            if (confirmUpdate) {
+                                var updatedCustomer = {
+                                    id: $('#update-customer-id').val(),
+                                    name: $('#update-customer-name').val(),
+                                    address: $('#update-customer-address').val(),
+                                    phone: $('#update-customer-tp').val()
+                                };
 
+                                $.ajax({
+                                    url: 'http://localhost:8080/possystem/customer?id=' + updatedCustomer.id,
+                                    type: 'PATCH',
+                                    contentType: 'application/json',
+                                    data: JSON.stringify(updatedCustomer),
+                                    success: function() {
+                                        $('#updateCustomerModal').modal('hide');
+                                        loadTable();
+                                    },
+                                    error: function(xhr, status, error) {
+                                        alert('Failed to update customer: ' + error);
+                                    }
+                                });
+                            }
+                        });
+                    }).appendTo(actionCell);
 
-                $('#update-customer-btn').off('click').on('click', function () {
+                // Delete button
+                $('<button>').text('Delete').addClass('btn btn-danger btn-sm')
+                    .on('click', function () {
+                        var confirmDelete = confirm('Are you sure you want to delete this customer?');
+                        if (confirmDelete) {
+                            $.ajax({
+                                url: 'http://localhost:8080/possystem/customer?id=' + customer.id,
+                                type: 'DELETE',
+                                success: function() {
+                                    loadTable();
+                                },
+                                error: function(xhr, status, error) {
+                                    alert('Failed to delete customer: ' + error);
+                                }
+                            });
+                        }
+                    }).appendTo(actionCell);
 
-                    var confirmUpdate = confirm('Are you sure you want to update this customer?');
-                    if (confirmUpdate) {
-                        customer.name = $('#update-customer-name').val();
-                        customer.address = $('#update-customer-address').val();
-                        customer.tp = $('#update-customer-tp').val();
-
-                        $('#updateCustomerModal').modal('hide');
-                        loadTable();
-                    }
-                });
-            }).appendTo(actionCell);
-
-        // Delete button
-        $('<button>').text('Delete').addClass('btn btn-danger btn-sm')
-            .on('click', function () {
-
-                validation();
-
-                var confirmDelete = confirm('Are you sure you want to delete this customer?');
-                if (confirmDelete) {
-                    customers.splice(index, 1);
-                    loadTable();
-                }
-            }).appendTo(actionCell);
-
-        row.appendTo(tableBody);
+                row.appendTo(tableBody);
+            });
+        },
+        error: function(xhr, status, error) {
+            alert('Failed to load customers: ' + error);
+        }
     });
 }
 
